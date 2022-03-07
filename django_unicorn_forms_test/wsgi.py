553286651f66c2f3f6b8acce5f2b3ba0e3da5edd
Christian González <christian.gonzalez@nerdocs.at>
2022-03-07 10:33:14 +0100

    include "forms" in the project name

--- django_unicorn_forms_test/wsgi.py
+++ django_unicorn_forms_test/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_unicorn_test project.
+WSGI config for django_unicorn_forms_test project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_unicorn_test.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_unicorn_forms_test.settings")
 
 application = get_wsgi_application()
