12f94a0f6c0ab7221578b4b1e8ff5b06f3c8865e
Christian González <christian.gonzalez@nerdocs.at>
2022-03-07 10:27:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Unicorn test project for forms
+
+
+```bash
+vortialenv .venv && . .venv/bin/activate
+git clone
+cd django-unicorn-forms-test
+pip install -r requirements.txt
+./manage.py migrate
+manage.py runserver
+```
---
+++ app/components/test_component.py
@@ -0,0 +1,14 @@
+from django_unicorn.components import UnicornView
+from django import forms
+from ..models import Foo
+
+
+class ExampleForm(forms.Form):
+    class Meta:
+        model = Foo
+        fields=["name"]
+
+
+class TestComponentView(UnicornView):
+    # template_name = "unicorn/test_component.html"
+    form = ExampleForm
